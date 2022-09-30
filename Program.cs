a2e99e262387a19c1faf62817f57fec66783a285
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-30 12:05:40 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+var videoGames = new List<string>()
+{ "Pokemon", "Skyrim", "Overwatch", "Fire Emblem", "Zelda", "Apex"};
+
+var games = videoGames.OrderBy(game => game.Length).ToList();
+
+//foreach (var videoGame in games)
+//{
+//    Console.WriteLine(videoGame);
+//}
+
+games.ForEach(Console.WriteLine);
